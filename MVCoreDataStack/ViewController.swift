5a53ae53b61f44d37a2015fd81f6e598d4f47f84
Andrea Bizzotto <bizz84@gmail.com>
2015-10-23 07:58:08 +0100

    Initial Commit

---
+++ MVCoreDataStack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVCoreDataStack
+//
+//  Created by Andrea Bizzotto on 23/10/2015.
+//  Copyright © 2015 musevisions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
